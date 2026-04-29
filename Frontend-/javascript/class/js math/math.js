082a32b99b7e896c39ec01add5ee03c196659444
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-04-29 09:19:46 +0530

    ..

---
+++ Frontend-/javascript/class/js math/math.js
@@ -0,0 +1,15 @@
+console.log(Math.PI)//print value of pi
+console.log(Math.sqrt(49))//print the square root of mumber
+console.log(Math.pow(9,2))//print the power of number
+console.log(Math.round(5.6))//round to nearest mumber
+console.log (Math.floor(2.5))//floor it to nearest intager value
+console.log (Math.trunc(239389.48727))//it will print intagar value
+        //and remove the decimal value
+console.log(Math.sign(-93819))
+console.log(Math.sign(1773))//print the mumber is megative pr positive
+console.log (Math.random())//print the random mumbers
+
+let res1=Math.random()*6+1//print the random mumber between 1 to 6
+console.log(res1)
+let res=Math.trunc(Math.random()*100+1)//print the random mumber between 1 to 100
+console.log(res)
