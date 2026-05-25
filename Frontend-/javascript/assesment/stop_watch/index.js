c2613d037e2518133bd2c1b3b45288c9da09df34
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-05-25 09:07:58 +0530

    stop watch

--- Frontend-/javascript/assesment/stop_watch/index.js
+++ Frontend-/javascript/assesment/stop_watch/index.js
@@ -8,5 +8,41 @@ let reset = document.getElementById('reset')
 let hours = 0
 let minutes = 0
 let seconds = 0
+let timer = null
 
-let timer
+function updateDisplay() {
+    hourSpan.innerText = hours.toString().padStart(2, '0')
+    minuteSpan.innerText = minutes.toString().padStart(2, '0')
+    secondSpan.innerText = seconds.toString().padStart(2, '0')
+}
+
+start.addEventListener('click', () => {
+    if (!timer) {
+        timer = setInterval(() => {
+            seconds++
+            if (seconds === 60) {
+                seconds = 0
+                minutes++
+            }
+            if (minutes === 60) {
+                minutes = 0
+                hours++
+            }
+            updateDisplay()
+        }, 1000)
+    }
+})
+
+stop.addEventListener('click', () => {
+    clearInterval(timer)
+    timer = null
+})
+
+reset.addEventListener('click', () => {
+    clearInterval(timer)
+    timer = null
+    hours = 0
+    minutes = 0
+    seconds = 0
+    updateDisplay()
+})
