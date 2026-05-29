001327ee10f46fcd42fd8cf9cfc3694d5c0115e0
Santhosh G P <gpsanthoshsanthu@gmail.com>
2026-05-29 13:51:32 +0530

    ...

--- Frontend-/React_JS/assignment/demo/src/App.jsx
+++ Frontend-/React_JS/assignment/demo/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Mydetail from './Model/Mydetail'
+import './App.css';
+
 
 function App() {
   return (
     <div>
+      <center>
+        <hr />
       <h1>hi hello i am santhosh g p</h1>
       <Mydetail/>
+      </center>
     </div>
   )
 }
--- Frontend-/React_JS/assignment/demo/src/Model/Mydetail.jsx
+++ Frontend-/React_JS/assignment/demo/src/Model/Mydetail.jsx
@@ -6,13 +6,13 @@ function Mydetail() {
     }
   return (
     <div>
-      <center>
-      <p>age : 22</p>
-      <p>phone number: 8147422441</p>
-      <p>current date:{ new Date().toLocaleDateString()}</p>
+      <hr />
+        <p><b>name:</b> santhosh g p</p>
+      <p><b>age</b> : 22</p>
+      <p><b>phone number:</b> 8147422441</p>
+      <p><b>current date:</b>{ new Date().toLocaleDateString()}</p>
           <img src={Photo} alt="photo" width="200" /><br />
       <button onClick={handleclick}>click</button>
-      </center>
     </div>
 
   )
